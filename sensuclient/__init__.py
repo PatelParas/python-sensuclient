d3cff3353c9793c5d01cdfd4a5168c7dcc8f1ce7
Paras Patel <ppatel@ebates.com>
2017-10-25 21:38:55 -0700

    Add python module for sensu check silence/unsilence

--- sensuclient/__init__.py
+++ sensuclient/__init__.py
@@ -5,6 +5,7 @@ class Client(object):
         self.host = host
         self.clients = ClientManager(self.host + '/clients')
         self.checks = CheckManager(self.host + '/checks')
+        self.silences = SilenceManager(self.host + '/silenced')
 
 class ResourceManager(object):
     def __init__(self, prefix):
@@ -32,7 +33,7 @@ class ResourceManager(object):
 class ClientManager(ResourceManager):
     def list(self):
         return self._get('')
-    
+
     def get(self, name):
         return self._get('/%s' % name)
 
@@ -52,3 +53,36 @@ class CheckManager(ResourceManager):
             'subscribers': subscribers,
         }
         return self._post('/request', data=payload)
+
+class SilenceManager(ResourceManager):
+    def list(self):
+        return self._get('')
+
+    def get(self, name):
+        return self._get('/%s' % name)
+
+    def silenceBySubscription(self, subscription, expire):
+        payload = {
+            'subscription': subscription,
+            'expire': expire,
+        }
+        return self._post('', json=payload)
+
+    def silenceByClientName(self, clientname, expire):
+        payload = {
+            'subscription': "client:" + clientname,
+            'expire': expire,
+        }
+        return self._post('', json=payload)
+
+    def clearBySubscription(self, subscription):
+        payload = {
+            'subscription':  subscription
+        }
+        return self._post('/clear', json=payload)
+
+    def clearByClientName(self, clientname):
+        payload = {
+            'subscription': "client:" + clientname
+        }
+        return self._post('/clear', json=payload)
